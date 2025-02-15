a77285eb39a14ba676f900d7c4759ecf8481424e
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-15 17:12:29 +0500

    Delete Boolean8.py

--- Boolean8.py
+++
@@ -1,7 +0,0 @@
-num=float(input())
-if bool(num>=10 and num<=20):
-    print("{} is between 10 and 20".format(num))
-elif  num>20:
-    print("{} is greater than 20".format(num))
-elif num<10:
-    print("{} is less than 10".format(num))
