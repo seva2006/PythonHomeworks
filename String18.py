223809bd911935125c0692477dee12b78d2500ec
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-15 17:13:36 +0500

    Delete String18.py

--- String18.py
+++
@@ -1,8 +0,0 @@
-Input=str(input("Enter a string:"))
-Input1=Input.split()
-if bool(Input1[0]==Input1[-1]):
-    print("Input string starts and ends with the same word")
-else:
-    print("Starts with: {}".format(Input1[0]))
-    print("Ends with: {}".format(Input1[-1]))
-
