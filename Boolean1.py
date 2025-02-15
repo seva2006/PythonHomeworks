536a445d96c2c9c791cbcccb7eabde5e09477181
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-15 17:09:03 +0500

    Delete Boolean1.py

--- Boolean1.py
+++
@@ -1,6 +0,0 @@
-username=input()
-password=input()
-if not bool(username.strip() and password.strip()):
-    print("Username and password are empty!")
-else:
-    print("Username: {}\nPassword: {} ".format(username,password))
