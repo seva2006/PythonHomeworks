f6c8cba57e8c009f8f102b57e93e93ea1f00ffba
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-15 16:59:23 +0500

    Delete lesson-1/lesson-2/homework/Boolean3.py

--- lesson-1/lesson-2/homework/Boolean3.py
+++
@@ -1,9 +0,0 @@
-num=float(input())
-if bool(num>0 and num%2==0 ):
-    print("{} is positive and even".format(num))
-elif num>0 and num%2!=0:
-    print("{} is positive but not even".format(num))
-elif num<0 and num%2==0:
-    print("{} is not positive and even".format(num))
-else:
-    print("{} is not positive and not even".format(num))
