e148d4c23aa1758a2fbb6713dbb43458713ea259
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-15 17:00:30 +0500

    Delete lesson-1/lesson-2/homework/Boolean5.py

--- lesson-1/lesson-2/homework/Boolean5.py
+++
@@ -1,6 +0,0 @@
-str1=str(input())
-str2=str(input())
-if bool(len(str1)==len(str2)):
-    print("{} and {} have the same length".format(str1,str2))
-else:
-    print("{} and {} have different length".format(str1,str2))
