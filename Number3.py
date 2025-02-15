e08f40c8b228cfa4a1c9438b37f453cc33fba156
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-15 17:12:57 +0500

    Delete Number3.py

--- Number3.py
+++
@@ -1,8 +0,0 @@
-INPUT=float(input())
-meters=INPUT*1000
-centimeters=INPUT*100000
-print("{} kilometers={} meters".format(INPUT,meters))
-print("{} kilometers={} centimeters".format(INPUT,centimeters))
-
-
-
