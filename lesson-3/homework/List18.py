4b1832a3310304606db1412b884a5f327d446ca2
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-17 01:45:02 +0500

    Update List18.py

--- lesson-3/homework/List18.py
+++ lesson-3/homework/List18.py
@@ -1,3 +1,7 @@
-list=[1,2,3,4,5,6,7,8,9,10]
-sublist=[3,4,6,7,9]
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sublist = [3, 4, 5]
+if any(list[i:i+len(sublist)] == sublist for i in range(len(list) - len(sublist) + 1)):
+    print("Sublist exists in the list")
+else:
+    print("Sublist does not exist in the list")
 
