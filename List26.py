536c2383bd7e843ef8e898100984dbf86e2dc064
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-17 00:47:27 +0500

    Delete List26.py

--- List26.py
+++
@@ -1,6 +0,0 @@
-list=[1,45,12,30,10,21,87,-25,31]
-index=len(list)//2
-if len(list)%2==0:
-    print("Middle elements are: {} {}".format(list[index-1],list[index]))
-else:
-    print("Middle element is {}".format(list[index]))
