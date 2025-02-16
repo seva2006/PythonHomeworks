b03c8f504798938dd3ac5de5ae3a9c00e0954b5f
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-17 00:38:58 +0500

    Delete Dictionary11.py

--- Dictionary11.py
+++
@@ -1,8 +0,0 @@
-person = {'name': 'John', 'age': 30, 'profession': 'teacher'}
-key=input()
-new_value=input()
-if key in person:
-    person[key] = new_value
-    print(f"Updated dictionary: {person}")
-else:
-    print("Key doesn't exist.")
