8bbe4864418a8e6012566684199bb064c54c7c86
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-17 00:38:48 +0500

    Delete Dictionary10.py

--- Dictionary10.py
+++
@@ -1,7 +0,0 @@
-person={'name':"John",'age':30,'profession':"teacher"}
-key=input()
-value = person.get(key)
-if value is not None:
-    print(f"The key-value pair is: {key}: {value}")
-else:
-    print("Key doesn't exist.")
