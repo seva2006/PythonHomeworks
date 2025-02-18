f839ae4b8a3a84010422481fd4c5647e7b39bafd
seva2006 <147371793+seva2006@users.noreply.github.com>
2025-02-18 23:17:45 +0500

    Update problem4.py

--- lesson-4/homework/problem4.py
+++ lesson-4/homework/problem4.py
@@ -6,7 +6,6 @@ attempts = 10
 for attempt in range(1, attempts + 1):
     guess = int(input(f"Attempt {attempt}: Guess a number between 1 and 100: "))
 
-    # Check the guess
     if guess > number_to_guess:
         print("Too high!")
     elif guess < number_to_guess:
